25a67e47ae1c97c9e6fdf35f182d41a469f6a0b1
zouxiaopo <zouxp@yiche.com>
2020-10-30 19:05:22 +0800

    Callable

--- src/main/java/com/zxp/helloplus/controller/AsyncController.java
+++ src/main/java/com/zxp/helloplus/controller/AsyncController.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
+import java.util.concurrent.Callable;
 
 /**
  * @author zxp
@@ -29,5 +31,21 @@ public class AsyncController {
         return "task任务总耗时:"+(currentTimeMillis1-currentTimeMillis)+"ms "+new Random().nextInt(100);
     }
 
-
+    @RequestMapping(value = "/email/callableReq")
+    @ResponseBody
+    public Callable<String> callableReq () {
+        System.out.println("外部线程：" + Thread.currentThread().getName());
+        Callable r= new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(10000);
+                System.out.println("内部线程：" + Thread.currentThread().getName());
+                return "callable!";
+            }
+        };
+        log.info("1");
+        log.info("2");
+        log.info("3");
+        return  r;
+    }
 }
